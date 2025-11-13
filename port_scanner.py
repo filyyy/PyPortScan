e710c74133f5b4e9c6a6dc0a2d438572820f0e16
filyyy <tondofilippo1@gmail.com>
2025-11-13 13:45:54 +0100

    implemented argparser and input validation

--- port_scanner.py
+++ port_scanner.py
@@ -1,6 +1,4 @@
-import math
-import socket
-import sys
+import math, socket, sys, argparse
 from threading import Thread, Lock
 
 common_ports =  [20, 21, 22, 23, 25, 53, 67, 68, 69, 80, 110, 111, 123, 135, 137, 138, 139,
@@ -9,6 +7,29 @@ common_ports =  [20, 21, 22, 23, 25, 53, 67, 68, 69, 80, 110, 111, 123, 135, 137
 
 open_ports = []
 lock = Lock()
+
+def valid_ports(ports_list):
+    valids = []
+    for port in ports_list:
+        if port < 1 or port > 65535:
+            print("Ports must be in range [1-65535]!")
+            sys.exit(1)
+        valids.append(port)
+    return valids
+
+def valid_range(ports_range):
+    valids = []
+    try:
+        start, stop = map(int, ports_range.split("-"))
+        if start < 1 or stop > 65535:
+            print("Ports must be in range [1-65535]!")
+            sys.exit(1)
+    except ValueError:
+        print("Invalid range! (python3 port_scanner.py [ip] -r [start-stop])")
+        sys.exit(1)
+    for port in range(start, stop+1):
+        valids.append(port)
+    return valids
 
 
 def threads_calc(length, bal=20, max_threads=100):
@@ -32,11 +53,14 @@ def scan_range(ip, ports):
                     open_ports.append(port)
             sock.close()
 
-def port_scan(ip, common_in_range, ports):
-    ports = [port for port in ports if port not in common_ports]
+def port_scan(ip, ports=common_ports):
+    common_in_range = [common for common in common_ports if common in ports]
+    ports = [port for port in ports if port not in common_in_range]
+
     threads = []
-    threads_count =  threads_calc(len(ports)+len(common_ports))
-    chunk_size = int((len(ports)+len(common_ports))/threads_count)
+    threads_count =  threads_calc(len(ports)+len(common_in_range))
+
+    chunk_size = int((len(ports)+len(common_in_range))/threads_count)
 
     for ports_chunk in list(common_in_range[i:i+chunk_size] for i in range(0,len(common_in_range),chunk_size)):
         t = Thread(target=scan_range, args=(ip, ports_chunk))
@@ -55,26 +79,32 @@ def port_scan(ip, common_in_range, ports):
         t.join()
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Specify the target ip! (python3 multithread_port_scanner.py [ip] [start-stop]")
-        sys.exit(1)
 
-    if len(sys.argv) == 3:
-        try:
-            start, stop = map(int, sys.argv[2].split("-"))
-        except ValueError:
-            print("Invalid range! (python3 multithread_port_scanner.py [ip] [start-stop])")
-            sys.exit(1)
-        ports = list(range(start, stop + 1))
-    elif len(sys.argv) == 2:
-        ports = common_ports
+    ports = []
 
-    ip = sys.argv[1]
-    common_in_range = [common for common in common_ports if common in ports]
-    ports = [port for port in ports if port not in common_in_range]
+    parser = argparse.ArgumentParser(prog="Port Scanner", description="Simple python TCP port scanner")
+    parser.add_argument("ip")
+    parser.add_argument("-r", "--range", default=None, type=valid_range, help="Scan ports in the given range")
+    parser.add_argument("-p", "--ports", default=None, type=int, nargs="+", help="Scan the given port list")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
+
+    args = parser.parse_args()
+
+    if args.range:
+        for port in args.range:
+            ports.append(port)
+    if args.ports:
+        for port in valid_ports(args.ports):
+            if port not in ports:
+                ports.append(port)
+
+    ip = args.ip
 
     print("Open ports: ", end="", flush=True)
 
-    port_scan(ip, common_in_range, ports)
+    if ports:
+        port_scan(ip, ports)
+    else:
+        port_scan(ip)
 
     print(f"\nNumber of open ports: {len(open_ports)}")
